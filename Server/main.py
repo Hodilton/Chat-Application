7de30925fdf5e48c97753dbd0783fcc9e39ae633
Hodilton <120922249+Hodilton@users.noreply.github.com>
2025-04-08 15:24:05 +0700

    Update main.py

--- Server/main.py
+++ Server/main.py
@@ -1,28 +1,27 @@
-from file_work.src.file_processor import FileProcessor
-from file_work.utils.utilities import Messages
+from file_work.utils.utilities import Messages as MessagesFile
+from database.utils.utilities import Messages as MessagesDB
 from logger.log_manager import LogManager
+
+from database.src.database import Database
 
 def main():
     log_config = {
         "file_work": "logs/file.log",
+        "db_work": "logs/database.log",
     }
 
     LogManager.init_loggers(log_config)
-    Messages.set_logger(LogManager.get_logger("file_work"))
+    MessagesFile.set_logger(LogManager.get_logger("file_work"))
+    MessagesDB.set_logger(LogManager.get_logger("db_work"))
+
+    LogManager.clear_log_for_all()
     LogManager.log_program_start_for_all()
 
-    config_path = {
-        'folder_path': "./data",
-        'file_name': 'tables_config',
-        'extension': 'json'
-    }
+    db = Database("./database/data")
+    db.connect()
+    db.reset()
 
-    data = {
-        "2": "1"
-    }
-
-    FileProcessor.write_file(config_path, data, True)
-
+    db.close()
     LogManager.log_program_end_for_all()
 
 if __name__ == "__main__":
