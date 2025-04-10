6f3c501a53043063af6aa2cbbc6c9550cb915713
Hodilton <120922249+Hodilton@users.noreply.github.com>
2025-04-10 21:14:56 +0700

    запрос всех пользователей

--- Client/app/src/main/java/com/example/android_app/network/UserRequests.java
+++ Client/app/src/main/java/com/example/android_app/network/UserRequests.java
@@ -1,17 +1,31 @@
 package com.example.android_app.network;
 
+import android.content.Context;
 import android.util.Log;
 
 import com.example.android_app.models.User;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import java.util.ArrayList;
 import java.util.List;
 
 public class UserRequests extends ApiRequest {
-    private static final String TAG = "UserRequests";
+    private static final String TAG = UserRequests.class.getSimpleName();
 
+    public interface UsersResponseCallback {
+        void onResponse(boolean success, String message, List<User> users);
+    }
 
+    public static void getAllUsers(Context context, UsersResponseCallback callback) {
+        String url = ServerConfig.BASE_URL + "/users";
+        try {
+            sendRequest(context, url, null, "GET",
+                    (success, message, response) -> runOnUiThread(context, () -> {
+                        List<User> users = success ? User.listFromJson(response) : null;
+                        callback.onResponse(success, message, users);
+
+                    }));
+        } catch (Exception e) {
+            Log.e(TAG, "Get users error", e);
+            callback.onResponse(false, "Get users request creation failed.", null);
+        }
+    }
 }
