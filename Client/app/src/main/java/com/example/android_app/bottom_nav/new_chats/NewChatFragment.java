44e1b33c1fce04de9021f6fc76a46bc05f2320dc
Hodilton <120922249+Hodilton@users.noreply.github.com>
2025-04-10 21:13:14 +0700

    прокинул адаптер

--- Client/app/src/main/java/com/example/android_app/bottom_nav/new_chats/NewChatFragment.java
+++ Client/app/src/main/java/com/example/android_app/bottom_nav/new_chats/NewChatFragment.java
@@ -4,13 +4,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.example.android_app.bottom_nav.BaseFragment;
 import com.example.android_app.databinding.FragmentNewChatBinding;
+import com.example.android_app.adapters.UserAdapter;
+import com.example.android_app.network.UserRequests;
+
+import java.util.ArrayList;
 
 public class NewChatFragment extends BaseFragment<FragmentNewChatBinding> {
+    private UserAdapter userAdapter;
+
     @Override
     protected FragmentNewChatBinding inflateBinding(LayoutInflater inflater, ViewGroup container) {
         return FragmentNewChatBinding.inflate(inflater, container, false);
@@ -19,5 +27,26 @@ public class NewChatFragment extends BaseFragment<FragmentNewChatBinding> {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        setupAdapter();
+        loadUsers();
+    }
+
+    private void setupAdapter() {
+        userAdapter = new UserAdapter(requireContext(), new ArrayList<>());
+        binding.usersLv.setAdapter(userAdapter);
+    }
+
+    private void loadUsers() {
+        setLoadingState(true);
+        UserRequests.getAllUsers(requireContext(),
+                (success, message, users) -> {
+            setLoadingState(false);
+            if (success && users != null) {
+                userAdapter.updateList(users);
+            } else {
+                Toast.makeText(requireContext(), "Error: " + message, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
