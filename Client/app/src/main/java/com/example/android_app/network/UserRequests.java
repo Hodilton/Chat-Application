c2b67baf8d70ee457c21e61ed9f3bd7aa5835103
Hodilton <120922249+Hodilton@users.noreply.github.com>
2025-04-14 02:10:01 +0700

    запрос для exclude

--- Client/app/src/main/java/com/example/android_app/network/UserRequests.java
+++ Client/app/src/main/java/com/example/android_app/network/UserRequests.java
@@ -28,4 +28,18 @@ public class UserRequests extends ApiRequest {
             callback.onResponse(false, "Get users request creation failed.", null);
         }
     }
+
+    public static void getAllUsersExclude(Context context, int excludeUserId, UsersResponseCallback callback) {
+        String url = ServerConfig.BASE_URL + "/users/exclude/" + excludeUserId;
+        try {
+            sendRequest(context, url, null, "GET",
+                    (success, message, response) -> runOnUiThread(context, () -> {
+                        List<User> users = success ? User.listFromJson(response) : null;
+                        callback.onResponse(success, message, users);
+                    }));
+        } catch (Exception e) {
+            Log.e(TAG, "Get users error", e);
+            callback.onResponse(false, "Get users request failed.", null);
+        }
+    }
 }
