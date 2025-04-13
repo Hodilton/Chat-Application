3cf8a5e6c3b0599b342e346a38505dddfec3c369
Hodilton <120922249+Hodilton@users.noreply.github.com>
2025-04-13 23:09:38 +0700

    подтянул chats_bp

--- Server/flask_app/app.py
+++ Server/flask_app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from .routes.users import users_bp, init_users_routes
+from .routes.chats import chats_bp, init_chats_routes
 
 app = Flask(__name__)
 db_global = None
@@ -9,5 +10,7 @@ def init_db(db):
     db_global = db
 
     init_users_routes(db)
+    init_chats_routes(db)
 
     app.register_blueprint(users_bp)
+    app.register_blueprint(chats_bp)
