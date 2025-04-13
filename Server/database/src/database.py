45431bcfce2afb3bc73e3dc2e9c1b0e7fe00cc0a
Hodilton <120922249+Hodilton@users.noreply.github.com>
2025-04-13 23:09:43 +0700

    Update database.py

--- Server/database/src/database.py
+++ Server/database/src/database.py
@@ -5,36 +5,6 @@ from ..connection.db_connection import DBConnection
 from ..services.db_handler import DBHandler
 from ..core.table import TablesContainer
 
-'''
-    # Добавляем автора по ФИО
-    db.tables.authors.insert(("Достоевский", "Михайлович", "Фёдор"))
-    id = db.tables.authors.insert(("Толстой", "Николаевич", "Лев"))
-
-    # Обновляем автора с id=1
-    is_success = db.tables.authors.update(("Толстой", "Николаевич", "Лев"), (1,))
-
-    # Удаляем автора с id=1
-    db.tables.authors.delete((1,))
-
-    # 1. Получить автора по ID
-    author = db.tables.authors.fetch("by_id", "one", (1,))
-
-    # 2. Получить всех авторов
-    all_authors = db.tables.authors.fetch("all", "all")
-
-    # 3. Найти ID автора по ФИО (первая запись, если не уникальны)
-    author_id = db.tables.authors.fetch("id_by_full_name", "one", ("Толстой", "Николаевич", "Лев"))
-
-    # 4. Найти авторов по ФИО
-    authors = db.tables.authors.fetch("by_full_name", "all", ("Толстой", "Николаевич", "Лев"))
-
-    # 5. Поиск по части имени (LIKE)
-    authors = db.tables.authors.fetch("by_partial_name", "all", ("%Толст%", "%Никол%", "%Лев%"))
-
-    # 6. Получить количество авторов
-    count = db.tables.authors.fetch("count", "one")[0]
-'''
-
 class Database:
     def __init__(self, config_path: str):
         self._db_config_loader = DBConfigLoader(config_path)
