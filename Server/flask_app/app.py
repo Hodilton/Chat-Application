1c1f301f38b6a7c29e86165948bd01a5989df372
Hodilton <120922249+Hodilton@users.noreply.github.com>
2025-04-16 03:59:00 +0700

    подтянул новый bp

--- Server/flask_app/app.py
+++ Server/flask_app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from .routes.users import users_bp, init_users_routes
 from .routes.chats import chats_bp, init_chats_routes
+from .routes.messages import messages_bp, init_messages_routes
 
 app = Flask(__name__)
 db_global = None
@@ -11,6 +12,8 @@ def init_db(db):
 
     init_users_routes(db)
     init_chats_routes(db)
+    init_messages_routes(db)
 
     app.register_blueprint(users_bp)
     app.register_blueprint(chats_bp)
+    app.register_blueprint(messages_bp)
