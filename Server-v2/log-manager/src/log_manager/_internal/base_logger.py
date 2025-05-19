43a9c091cb6ccd1391e768baa463bcef94627e63
Hodilton <120922249+Hodilton@users.noreply.github.com>
2025-05-19 07:38:39 +0700

    nope

--- Server-v2/log-manager/src/log_manager/_internal/base_logger.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-class LogFormatter:
-    DEFAULT_FORMAT = "[%(asctime)s] %(levelname)s: %(message)s"
-    DEFAULT_DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
-
-    @staticmethod
-    def get_formatter():
-        return logging.Formatter(LogFormatter.DEFAULT_FORMAT, LogFormatter.DEFAULT_DATE_FORMAT)
--- Server-v2/log-manager/src/log_manager/_utils/directory.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Directory:
-    @staticmethod
-    def ensure_dir_for_file(path: str) -> None:
-        directory = os.path.dirname(path)
-        if directory and not os.path.exists(directory):
-            os.makedirs(directory, exist_ok=True)
