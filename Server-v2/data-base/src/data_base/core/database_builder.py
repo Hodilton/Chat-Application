f316883245cc2cf815c6d00f73c38dba78619f6e
Hodilton <120922249+Hodilton@users.noreply.github.com>
2025-06-12 22:37:11 +0700

    Update database_builder.py

--- Server-v2/data-base/src/data_base/core/database_builder.py
+++ Server-v2/data-base/src/data_base/core/database_builder.py
@@ -1,13 +1,13 @@
-from .._bootstrap.database_bootstrapper import DatabaseBootstrapper
+from data_base._config.config_bootstrapper import ConfigBootstrapper
 from .._infrastructure.aiomysql.aiomysql_connection import AioMySQLConnection
-from .._services.database_wrapper import DatabaseWrapper
+from .._infrastructure.database_wrapper import DatabaseWrapper
 
 class DatabaseBuilder:
     def __init__(self, config_path: str):
         self._config_path = config_path
 
     async def build(self) -> DatabaseWrapper:
-        bootstrapper = DatabaseBootstrapper(self._config_path)
+        bootstrapper = ConfigBootstrapper(self._config_path)
         if not bootstrapper.bootstrap():
             raise RuntimeError("Bootstrap failed")
 
