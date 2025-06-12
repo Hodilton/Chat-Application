5a3d477e1803e735848eff4b49a84ee769d3870a
Hodilton <120922249+Hodilton@users.noreply.github.com>
2025-06-12 22:36:38 +0700

    correct typing

--- Server-v2/data-base/src/data_base/_infrastructure/aiomysql/aiomysql_repository.py
+++ Server-v2/data-base/src/data_base/_infrastructure/aiomysql/aiomysql_repository.py
@@ -1,21 +1,30 @@
-from typing import Any, Tuple
+from typing import Any, Tuple, Optional, List
 import aiomysql
+from .._shared.messages.msg_db import MsgDataBase
 
 class AioMySQLRepository:
-    def __init__(self, pool, queries: dict):
+    def __init__(self, pool: aiomysql.Pool, queries: dict) -> None:
         self._pool = pool
         self._queries = queries
 
     async def execute(self, query: str, params: Tuple = ()) -> int:
-        async with self._pool.acquire() as conn:
-            async with conn.cursor() as cur:
-                await cur.execute(query, params)
-                return cur.rowcount
+        try:
+            async with self._pool.acquire() as conn:
+                async with conn.cursor() as cur:
+                    await cur.execute(query, params)
+                    return cur.rowcount
+        except Exception as e:
+            MsgDB.Failure.query_failed(query, str(e))
+            return -1
 
     async def fetch(self, query: str, params: Tuple = (), fetch_mode="one") -> Any:
-        async with self._pool.acquire() as conn:
-            async with conn.cursor(aiomysql.DictCursor) as cur:
-                await cur.execute(query, params)
-                if fetch_mode == "one":
-                    return await cur.fetchone()
-                return await cur.fetchall()
+        try:
+            async with self._pool.acquire() as conn:
+                async with conn.cursor(aiomysql.DictCursor) as cur:
+                    await cur.execute(query, params)
+                    if fetch_mode == "one":
+                        return await cur.fetchone()
+                    return await cur.fetchall()
+        except Exception as e:
+            MsgDB.Failure.query_failed(query, str(e))
+            return None
